cabf42712ccf7f701054b6026917d652a4db7e74
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2024-11-27 12:41:03 +0800

    fix title

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Servey | Pejabat Daerah dan Tanah Alor Gajah</title>
+    <title>Survey | Pejabat Daerah dan Tanah Alor Gajah</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
